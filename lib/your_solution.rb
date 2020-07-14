29e398dc9f094fb35a64d7fa18a07c75852f82f4
Lida Attenborough <attenboroughl@protonmail.com>
2020-07-14 14:37:58 +0000

    Automatically backed up by Learn

--- lib/your_solution.rb
+++ lib/your_solution.rb
@@ -1,7 +1,4 @@
 # Write your solution here
 puts "Hell World!"
 # This file should output "Hello World!" using a puts statement
-# ./lib/your_solution.rb
-puts "Hello World!"
-# ./lib/your_solution.rb
-puts "Hello World!"
+
